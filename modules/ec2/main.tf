0e3be1799b3be6d0070b8c5a60f262a934eca9bc
chypwc <chienyeh5@gmail.com>
2025-07-03 23:13:55 +1000

    attach instance role

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -92,6 +92,7 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids      = var.bastion_security_group_ids
   key_name                    = var.key_name
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
 
   user_data = file("${path.module}/bastion-init.sh")
 
